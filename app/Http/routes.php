8ceadff9e44ca0b157fcb6588a60562ce30ced60
mihaela <grigore_mihaela@ymail.com>
2015-07-21 18:39:51 +0300

    right posts route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,35 +12,38 @@
 */
 
 Route::get('/', 'PagesController@home');
-//Route::get('home', 'PagesController@home');
-//Route::get('/', ['as' => 'home', 'uses' => 'PagesController@home']); 
 
 /*
 |--------------------------------------------------------------------------
 | Authentication
 |--------------------------------------------------------------------------
 */
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
 
-
-//Route::bind('user', function(App\User $id) {
-//    return DB::table('users')->where('id', $id)->first();
-//	  return User::where('id', $id)->firstOrFail();  });
+/*
+|--------------------------------------------------------------------------
+| Users
+|--------------------------------------------------------------------------
+*/
 
 Route::resource('users', 'UsersController');
 //Route::get('users', 'UsersController@index'); 
 //Route::get('users/{id}', 'UsersController@show');
 //Route::get('users/{id}/edit', ['as' => 'edit', 'uses' =>'UsersController@edit']);
 
-//Route::get('users/{id}', function( $id)
-//{ return View::make('users.show'); });
+/*
+|--------------------------------------------------------------------------
+| Posts
+|--------------------------------------------------------------------------
+*/
 
 Route::resource('posts', 'PostsController');
-Route::post('posts/{id}/edit', 'PostsController@update');
 //Route::get('posts', 'PostsController@index');
 //Route::get('posts/create', 'PostsController@create'); 
 //Route::get('posts/{id}', 'PostsController@show');
 //Route::post('posts', 'PostsController@store');
+//Route::post('posts/{id}/edit', 'PostsController@update');
